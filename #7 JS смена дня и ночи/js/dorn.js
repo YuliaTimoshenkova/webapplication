dcb7894b080fd80550cf29b65c9ac9d766a8369e
Yulia <juliatimoshenkova@gmail.com>
2015-04-24 00:13:47 +0500

    JS смена дня и ночи

---
+++ #7 JS смена дня и ночи/js/dorn.js
@@ -0,0 +1,15 @@
+﻿var Pic = new Array()
+Pic[0] = 'd.jpg'
+Pic[1] = 'n.jpg'
+var hours = new Date().getHours();
+function runBGSlideShow() {
+    if (hours > 6 && hours < 19) {
+        document.body.background = Pic[0];
+    }
+    else {
+        document.body.background = Pic[1];
+    }
+}
+setInterval(check, 60000); // выполняется раз в минуту, чтобы не слишком нагружать
+check();
+
