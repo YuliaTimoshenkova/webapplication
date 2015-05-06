1c236c59db27af522fdf882c4b0ac8e1a7113e46
Yulia <juliatimoshenkova@gmail.com>
2015-05-06 21:37:27 +0500

    task 2

---
+++ #11 js ria/wh/model/product.js
@@ -0,0 +1,16 @@
+Ext.define('wh.model.Product', {
+    extend: 'Ext.data.Model',
+    requires: [
+        'Ext.data.reader.Json'
+    ],
+
+    fields: ['id',
+        {name: 'name',  type: 'string'},
+        {name: 'price',   type: 'int', convert: null},
+    ],
+    
+
+    changeName: function(value) {
+        this.set('name', value);
+    }
+});
